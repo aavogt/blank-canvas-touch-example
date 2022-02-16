4eb54794efce05b5019bda32bcb811d37d26173b
Adam Vogt <vogt.adam@gmail.com>
2022-02-15 19:36:45 -0500

    disable zooming

--- main.hs
+++ main.hs
@@ -3,8 +3,15 @@ module Main where
 import Graphics.Blank
 import Control.Monad
 
+disableZoom ct = send ct $ eval
+   "document.addEventListener(\"touchstart\",function(e){\
+        \if (e.touches.length > 1) { e.preventDefault(); }},{passive: false});"
+
 main = blankCanvas 3000{ events = ["mousedown", "mouseup", "mousemove" , "touchstart", "touchmove", "touchend", "touchcancel"],
    middleware = [],
-  debug = False } $ \ ct -> forever $ do
+  debug = False } $ \ ct -> do
+    disableZoom ct
+    forever $ do
         ev <- wait ct
         print ev
+
